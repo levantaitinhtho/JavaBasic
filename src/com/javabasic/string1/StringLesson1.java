3f6ee3a7238756c788223937546a8494011d947d
levantaitinhtho <levantaitinhtho@gmail.com>
2021-08-04 12:23:49 +0700

    init config1

--- src/com/javabasic/string1/StringLesson1.java
+++ src/com/javabasic/string1/StringLesson1.java
@@ -5,7 +5,18 @@ class StringLession1{
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             String s = sc.next();
-            System.out.println(" Vị trí của kí tự a là : "+ s.indexOf("a") + " và "+ s.lastIndexOf("a"));
+            System.out.println("- Vị trí của kí tự a là : "+ s.indexOf("a") + " và "+ s.lastIndexOf("a"));
+            /* Tách chuỗi ( Substring)*/
+            System.out.println("- Chuỗi sẽ trở thành :" + s.substring(0,3)+","+ s.substring(3,6)+","+s.substring(6,8)+","+s.substring(8));
+            /* Kết hợp chuỗi ( concat)*/
+            System.out.println("- Và khi nối chuỗi trên sẽ trở thành : "+ s.concat("."));
+            /* Lấy chuỗi trong chuỗi ( Substring)*/
+            System.out.println("- Khi lấy chuỗi -DaNang- ta được : "+ s.substring(6,8));
+            /* Thay thế chuỗi (replace)*/
+            System.out.println("- Thay thế Da Nang -->HaNoi ta được : "+ s.replace("DaNang","HaNoi"));
+            /* Kiểm tra chuỗi có rỗng or không */
+            System.out.println("- Khi kiểm tra chuỗi có rỗng hay không ta được giá trị : "+s.isEmpty());
+
         }
     }
 
